b4317e7a18dbc74b179dfd964c4acd050c1a1332
edayot <pro.e.dayot@gmail.com>
2021-05-21 15:12:01 +0200

    suite

--- output_creator.py
+++ output_creator.py
@@ -19,7 +19,7 @@ dt_string = now.strftime("_%d_%m_%Y__%H_%M_%S")
 
 serial_port = 'COM4'
 baud_rate = 4000000 #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "output"+dt_string+".txt"
+write_to_file_path = "output\\output"+dt_string+".txt"
 
 
 ser = serial.Serial(serial_port, baud_rate)
